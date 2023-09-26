021ce9e767350f52e3a3aa7a31fd29f331dfb4bb
patoche62500 <b.grebert62@gmail.com>
2023-09-26 19:09:17 +0200

    Ajout des data qui vienne du back

--- FrontEnd/fetch.js
+++ FrontEnd/fetch.js
@@ -1,7 +1,14 @@
+let data;
+
 export async function fetchWork() {
   const work = await fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
     .then((response) => response)
     .catch((error) => alert("Erreur : " + error));
-    console.log(work);
+  console.log(work);
+  data = work;
+}
+
+export function getData() {
+  return data;
 }
--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,7 +1,24 @@
 import { fetchWork } from "./fetch.js";
+import { getData } from "./fetch.js";
 
-async function init(){
-    await fetchWork();
+async function init() {
+  await fetchWork();
 }
 
-init();
+await init();
+
+console.log(getData());
+
+getData().forEach((element) => {
+  const container = document.querySelector(".gallery");
+  const containerimage = document.createElement("figure");
+  const image = document.createElement('img');
+  image.src = element.imageUrl;
+  image.alt = element.title;
+  containerimage.appendChild(image);
+  const texte = document.createElement('figcaption');
+  texte.innerHTML = element.title;
+  containerimage.appendChild(texte);
+  container.appendChild(containerimage);
+  //console.log(container);
+});
